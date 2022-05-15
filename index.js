99371bb694e7929dc265dc6b770cd9061825ee2c
Nayeem29 <nayeembracu29@gmail.com>
2022-05-16 00:20:52 +0600

    appointment and available api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,12 +16,48 @@ async function run() {
     await client.connect();
     console.log('database is connected');
     const serviceCollection = client.db('Doctor_Services').collection('Service_Slot');
+    const bookingCollection = client.db('Doctor_Services').collection('bookings');
 
     app.get('/services', async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+    // app.get('/booking', async (req, res) => {
+    //   const query = {};
+    //   const cursor = bookingCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // })
+    app.get('/available', async (req, res) => {
+      const date = req.query.date;
+      // console.log(date);
+      const services = await serviceCollection.find().toArray();
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+      services.forEach(service => {
+        const serviceBooking = bookings.filter(book => book.treatment === service.name);
+        const booked = serviceBooking.map(s => s.slot);
+        const available = service.slots.filter(s => !booked.includes(s));
+        service.slots = available;
+
+      });
+
+      res.send(services)
+    })
+    app.post('/booking', async (req, res) => {
+
+      const doc = req.body;
+      // console.log(doc);
+      const query = { treatment: doc.treatment, date: doc.date, patientEmail: doc.patientEmail }
+      const exist = await bookingCollection.findOne(query);
+      // console.log(exist);
+      if (exist) {
+        return res.send({ success: false, doc: exist })
+      }
+      const result = await bookingCollection.insertOne(doc);
+      return res.send({ success: true, result });
     })
 
   } finally {
