5091d6efabf25f14e00544b15d40458df6fa5eee
Nayeem29 <nayeembracu29@gmail.com>
2022-05-16 23:51:14 +0600

    jwt function and token verify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorized' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p7gut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,6 +33,7 @@ async function run() {
     console.log('database is connected');
     const serviceCollection = client.db('Doctor_Services').collection('Service_Slot');
     const bookingCollection = client.db('Doctor_Services').collection('bookings');
+    const userCollection = client.db('Doctor_Services').collection('users');
 
     app.get('/services', async (req, res) => {
       const query = {};
@@ -24,12 +41,31 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    // app.get('/booking', async (req, res) => {
-    //   const query = {};
-    //   const cursor = bookingCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // })
+    app.get('/booking', verifyJWT, async (req, res) => {
+      const patient = req.query.patient;
+      const decodedEmail = req.decoded.email;
+      if (decodedEmail === patient) {
+        const query = { patientEmail: patient };
+        const cursor = bookingCollection.find(query);
+        const result = await cursor.toArray();
+        return res.send(result);
+      } else {
+        return res.status(403).send({ message: 'Forbidden Access' });
+      }
+    });
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: user
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ result, accessToken: token });
+    })
     app.get('/available', async (req, res) => {
       const date = req.query.date;
       // console.log(date);
